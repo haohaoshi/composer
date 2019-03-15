41f2bb9573e0012b098d199c8e5f53127af2d99a
lujz <lujz@cnfol.net>
2019-03-15 10:13:44 +0800

    添加demo

---
+++ demo.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lujz
+ * Date: 2019\3\15 0015
+ * Time: 10:10
+ */
+namespace demo;
+class demo
+{
+    public static function run(){
+        echo 222;
+    }
+}
